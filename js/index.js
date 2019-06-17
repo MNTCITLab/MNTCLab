54235924db5424db4e222998453d9ccaa833b6eb
Ryan Le <31494954+lryanle@users.noreply.github.com>
2019-06-17 12:00:52 -0500

    added null

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
-window.onload = function() {
-  window.scrollTo(0, -30)
-};
+function certNames() {
+fetch('https://raw.githubusercontent.com/MNTCITLab/MNTCLab/master/certificationNames.json').then(r => r.json()).then(function (fetched) {
+ certNames = fetched;
+console.log(certNames);
+});
+}
+document.onload = certNames();
