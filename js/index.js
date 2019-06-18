527aa6f4827c8e8ade31a4dc58343d1ae5b3de7f
Ryan Le <lryanle@users.noreply.github.com>
2019-06-17 23:27:21 -0500

    Time + fluid scrolling names push

--- js/index.js
+++ js/index.js
@@ -1,7 +1,40 @@
-function certNames() {
-fetch('https://raw.githubusercontent.com/MNTCITLab/MNTCLab/master/certificationNames.json').then(r => r.json()).then(function (fetched) {
- certNames = fetched;
-console.log(certNames);
+function scroller() {
+    fetch('https://raw.githubusercontent.com/MNTCITLab/MNTCLab/master/certificationNames.json').then(r => r.json()).then(function (fetched) {
+        var string = "";
+        for (i = 0; i < fetched.length; i++) {
+            string += `<br><br><br><b><u>${fetched[i].certification}</u></b><br>`;
+            for (ii = 0; ii < fetched[i].names.length; ii++) {
+                string += `${fetched[i].names[ii]} <br>`;
+            }
+        }
+    document.getElementById('scrollingText').innerHTML = `${string}`;
 });
 }
-document.onload = certNames();
+document.onload = scroller();
+
+window.onload = function () {
+    clock();
+
+    function clock() {
+        var now = new Date();
+        var TwentyFourHour = now.getHours();
+        var hour = now.getHours();
+        var min = now.getMinutes();
+        var sec = now.getSeconds();
+        var mid = 'PM';
+        if (min < 10) {
+            min = "0" + min;
+        }
+        if (hour > 12) {
+            hour = hour - 12;
+        }
+        if (hour == 0) {
+            hour = 12;
+        }
+        if (TwentyFourHour < 12) {
+            mid = 'AM';
+        }
+        document.getElementById('clock').innerHTML = hour + ':' + min + ':' + sec + ' ' + mid;
+        setTimeout(clock, 1000);
+    }
+}
